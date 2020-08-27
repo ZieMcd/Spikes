637f6127877fa8671f726c961b4af961e800505a
Zie Mcdowell <ziemcd@gmail.com>
2020-08-27 23:10:38 +0200

    Adding some Python and java/html

---
+++ Python3/Logical_Operators/logic.py
@@ -0,0 +1,18 @@
+# has_high_income = True
+# has_good_credit = True
+
+# if has_high_income and has_good_credit:  #can also use or
+#     print("can get a loan")
+
+# has_criminal_record = False
+
+# if has_good_credit and not has_criminal_record:
+#     print("still eligible for loan")
+
+temp = 30
+
+if temp > 30:
+    print("it is a hot day")
+else:
+    print("it is a cold day")
+
---
+++ Python3/Loops/loops.py
@@ -0,0 +1,16 @@
+i = 1
+
+while i<=5:
+    print(i)
+    i = i +1
+
+print("Done") 
+
+for item in "Python":
+    print(item)
+
+for item in ['zie','jeff','my nan']:
+    print(item)
+
+for item in range(5,10)  #range has many overloads
+    print(item)
